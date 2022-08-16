9d8f651f0262384535dc6cb623539f63e83eef8a
JulianDavidd <110936466+JulianDavidd@users.noreply.github.com>
2022-08-16 15:31:55 -0500

    Add files via upload

---
+++ 20220816_calculadora/index.js
@@ -0,0 +1,21 @@
+//suma, multiplicacion, division, resta 
+
+function suma(){
+resultado = 5 + 6
+console.log(resultado) 
+}
+suma()
+
+function multiplicacion(){
+resultado = 5 x 4
+console.log(resultado)
+}
+multiplicacion()
+
+function suma(){
+    resultado = 20 + 4
+    console.log(resultado)
+}
+suma()
+
+suma()
